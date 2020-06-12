7720b1f1b2f4e81cc733ec29bb3b2ff9b90dbd36
LinardosPaschopoulos <54861165+LinardosPaschopoulos@users.noreply.github.com>
2020-06-12 13:43:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include "gl/glut.h"
 #include <iostream>
+#include <GL/glut.h>
 #include <math.h>
+
 using namespace std;
 
+float score1 = 0, score2 = 0;
 GLfloat xb1 = 1, yb1 = 1;
 GLfloat xb2 = 0, yb2 = 0;
 
@@ -10,10 +12,17 @@ static void display() {
     float theta, x, y, pi = 3.141592653589793;
 	int i = -1;
 
+	void text();
+
     glClearColor(0.9, 0.5, 0.05, 0); // orange for the space outside of the field
 	glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(0, 0.75, 0); // green for the field
+    if (xb2 < 61 && xb2 > -61) {
+        glColor3f(0, 0.75, 0); // green for the field
+    }
+    else {
+        glColor3f(1, 0, 0); // red field when a goal is scored
+    }
 	glRectf(-69, -54, 69, 54); // the green rectangular in which the field will be designed
 
     glColor3f(1, 1, 1); // white color for the filed lines and the ball
@@ -26,18 +35,25 @@ static void display() {
         glVertex2f(i * 60, 45);
         glEnd();
 
-        glBegin(GL_LINE_LOOP); // the penalty area will be created the first time this loop will be executed
+        glBegin(GL_LINE_LOOP); // the left penalty area will be created the first time this loop will be executed
         glVertex2f(i * 60, -20.15);
         glVertex2f(i * 43.5, -20.15);
         glVertex2f(i * 43.5, 20.15);
         glVertex2f(i * 60, 20.15);
         glEnd();
 
-        glBegin(GL_LINE_LOOP); // the goal area will be created the first time this loop will be executed
+        glBegin(GL_LINE_LOOP); // the left goal area will be created the first time this loop will be executed
         glVertex2f(i * 60, -9.15);
         glVertex2f(i * 54.5, -9.15);
         glVertex2f(i * 54.5, 9.15);
         glVertex2f(i * 60, 9.15);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); // the left goalkeeper's area will be created the first time this loop will be executed
+        glVertex2f(i * 62.44, -3.65);
+        glVertex2f(i * 60, -3.65);
+        glVertex2f(i * 60, 3.65);
+        glVertex2f(i * 62.44, 3.65);
         glEnd();
 
         theta = -22 * pi/75;
@@ -77,21 +93,51 @@ static void display() {
 }
 
 void specialKeys(int key, int a, int b) {
-	if (key == GLUT_KEY_UP) {
-		yb1++;
-		yb2++;
+	if (key == GLUT_KEY_RIGHT && xb2 <= 59) {
+		xb1++;
+		xb2++;
 	}
-	if (key == GLUT_KEY_DOWN) {
-		yb1--;
-		yb2--;
-	}
-	if (key == GLUT_KEY_LEFT) {
+    else if (key == GLUT_KEY_RIGHT && xb2 <= 61.44 && yb2 <= 3.65 && yb2 >= -3.65) {
+        xb1++;
+		xb2++;
+    }
+
+
+	if (key == GLUT_KEY_LEFT && xb2 >= -59) {
 		xb1--;
 		xb2--;
 	}
-	if (key == GLUT_KEY_RIGHT) {
-		xb1++;
-		xb2++;
+    else if (key == GLUT_KEY_LEFT && xb2 >= -61.44 && yb2 <= 3.65 && yb2 >= -3.65) {
+        xb1--;
+		xb2--;
+    }
+
+
+    if (key == GLUT_KEY_UP && yb2 <= 44 && xb2 <= 60.44 && xb2 >= -60.44) {
+		yb1++;
+		yb2++;
+	}
+    else if (key == GLUT_KEY_UP && yb2 <= 2.65 && xb2 <= -59) {
+		yb1++;
+		yb2++;
+	}
+    else if (key == GLUT_KEY_UP && yb2 <= 2.65 && xb2 >= 59) {
+		yb1++;
+		yb2++;
+	}
+
+
+	if (key == GLUT_KEY_DOWN && yb2 >= -44 && xb2 <= 60.44 && xb2 >= -60.44) {
+		yb1--;
+		yb2--;
+	}
+	else if (key == GLUT_KEY_DOWN && yb2 >= -2.65 && xb2 <= -59) {
+		yb1--;
+		yb2--;
+	}
+    else if (key == GLUT_KEY_DOWN && yb2 >= -2.65 && xb2 >= 59) {
+		yb1--;
+		yb2--;
 	}
 
 	glutPostRedisplay();
@@ -100,7 +146,7 @@ void specialKeys(int key, int a, int b) {
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(1080, 1080);
+	glutInitWindowSize(744, 744);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutCreateWindow(argv[0]);
 
